f8d6874db50287ae5a16e4e7cde12b1ff024f91b
zhtsu <zhtsu@qq.com>
2024-10-16 00:31:42 +0800

    Add map view control

--- level/level.gd
+++ level/level.gd
@@ -1,9 +1,44 @@
 extends Node
 
 
+@onready var map: Control = $Map
+
+const MinMapScale = 1.0
+const MaxMapScale = 2.0
+const MapScaleStep = 0.05
+const MapScaleTime = 0.1
+
+var mouse_button_right_down: bool = false
+var mouse_button_right_down_position: Vector2
+var map_position: Vector2
+
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			pass
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			pass
+	if Input.is_action_just_pressed("Pan"):
+		mouse_button_right_down = true
+		mouse_button_right_down_position = event.position
+		map_position = map.position
+	if Input.is_action_just_released("Pan"):
+		mouse_button_right_down = false
+		
+	if event is InputEventMouseMotion:
+		if mouse_button_right_down:
+			map.position = map_position + (event.position - mouse_button_right_down_position)
+		
+	var tween: Tween = get_tree().create_tween()
+	if Input.is_action_just_pressed("ZoomIn"):
+		tween.tween_property(map, "scale", Vector2(
+			min(map.scale.x + MapScaleStep, MaxMapScale),
+			min(map.scale.y + MapScaleStep, MaxMapScale)
+		), MapScaleTime)
+	elif Input.is_action_just_pressed("ZoomOut"):
+		tween.tween_property(map, "scale", Vector2(
+			max(map.scale.x - MapScaleStep, MinMapScale),
+			max(map.scale.y - MapScaleStep, MinMapScale)
+		), MapScaleTime)
+	else:
+		tween.kill()
+		
+		
+func _process(delta: float) -> void:
+	print_debug(map.pivot_offset)
